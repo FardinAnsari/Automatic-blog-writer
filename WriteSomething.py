6d372bf3b33aeb14340bd0cb31cb86561bdfa4a1
fardin ansari <56290736+FardinAnsari@users.noreply.github.com>
2020-12-19 21:48:12 -0800

    Add files via upload

---
+++ WriteSomething.py
@@ -0,0 +1,79 @@
+import json
+import os
+import numpy as np
+# import tensorflow as tf
+# from tensorflow.compat.v1 import Session
+import tensorflow.compat.v1 as tf
+import model, sample, encoder
+
+tf.disable_v2_behavior()
+
+os.environ['PYTHONIOENCODING'] = 'UTF-8'
+
+
+class HParams:
+    def __init__(self):
+        self.n_vocab = 50257
+        self.n_ctx = 1024
+        self.n_embd = 1024
+        self.n_head = 16
+        self.n_layer = 24
+
+
+def interact_model(model_name, seed, nsamples, batch_size, length, temperature, \
+                   top_k, models_dir):
+    models_dir = os.path.expanduser(os.path.expandvars(models_dir))
+    if batch_size is None:
+        batch_size = 1
+    assert nsamples % batch_size == 0
+
+    enc = encoder.get_encoder(model_name, models_dir)
+    # hparams = model.default_hparams()
+    # with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
+    #    hparams.override_from_dict(json.load(f))
+
+    hparams = HParams()
+
+    if length is None:
+        length = hparams.n_ctx // 2
+    elif length > hparams.n_ctx:
+        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+    with tf.Session(graph=tf.Graph()) as sess:
+        context = tf.placeholder(tf.int32, [batch_size, None])
+        np.random.seed(seed)
+        tf.set_random_seed(seed)
+        output = sample.sample_sequence(
+            hparams=hparams, length=length,
+            context=context,
+            batch_size=batch_size,
+            temperature=temperature, top_k=top_k
+        )
+
+        saver = tf.train.Saver()
+        ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
+        saver.restore(sess, ckpt)
+
+        while True:
+            try:
+                raw_text = input("Model prompt >>> ")
+            except KeyboardInterrupt:
+                exit()
+            while not raw_text:
+                print('Prompt should not be empty!')
+                raw_text = input("Model prompt >>> ")
+            context_tokens = enc.encode(raw_text)
+            generated = 0
+            for _ in range(nsamples // batch_size):
+                out = sess.run(output, feed_dict={
+                    context: [context_tokens for _ in range(batch_size)]
+                })[:, len(context_tokens):]
+                for i in range(batch_size):
+                    generated += 1
+                    text = enc.decode(out[i])
+                    print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
+                    print(text)
+            print("=" * 80)
+
+
+interact_model('345M', 56, 1, 1, 300, 1, 0, 'models')
